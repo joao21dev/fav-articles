f7db1b3289a2aa7f68babec518a1e2423095768e
joao21dev <joao21dev@gmail.com>
2022-09-16 00:37:53 -0300

    logout button ok

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -15,22 +15,39 @@ const SidebarNav = styled.div<{ sidebar: boolean }>`
   top: 0;
   left: ${({ sidebar }) => (sidebar ? '0' : '-100%')};
 `
+const SidebarWrap = styled.div`
+  height: 80%;
+`
 const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-start;
+  transition: 0.1s ease-in-out;
   &:hover {
-    background-color: #3638b8;
+    background-color: #282842;
     text-decoration: none;
     color: white;
   }
 `
+const LogoutButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  background-color: #282842;
+  width: 150px;
+  border-radius: 15px;
+  height: 50px;
+  cursor: pointer;
+  margin-left: 2rem;
 
-const SidebarWrap = styled.div``
+  &:hover {
+    opacity: 0.8;
+  }
+`
 
 const Sidebar: FC = () => {
   const [sidebar, setSidebar] = useState(false)
-  const showSidebar = () => setSidebar(!sidebar)
   const [loading, setLoading] = useState(false)
 
   const navigate = useNavigate()
@@ -62,6 +79,16 @@ const Sidebar: FC = () => {
             )
           })}
         </SidebarWrap>
+        <LogoutButton onClick={handleLogout}>
+          {' '}
+          {!loading && 'Logout'}
+          {loading && (
+            <span className='indicator-progress' style={{ display: 'block' }}>
+              Carregando...{' '}
+              <span className='spinner-border spinner-border-sm align-middle ms-2'></span>
+            </span>
+          )}
+        </LogoutButton>
       </SidebarNav>
     </>
   )
